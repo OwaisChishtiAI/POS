3df16934ff3f61d776062fb0e660a230439e2800
owais-visualytics <79832735+owais-visualytics@users.noreply.github.com>
2021-09-28 20:09:52 +0500

    first commit

---
+++ Store-POS-release-builds-POS-win32-x64-win32-x64/resources/app/build.js
@@ -0,0 +1,16 @@
+const electronInstaller = require('electron-winstaller');
+const path = require('path');
+
+const rootPath = path.join('./');
+
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: './Store-POS-release-builds-POS-win32-x64-win32-x64',
+    outputDirectory: './exes',
+    authors: 'Hosting Domain',
+    noMsi: true,
+    exe: 'Store-POS-release-builds-POS-win32-x64.exe',
+    setupExe: 'POSInstaller.exe',
+    setupIcon: path.join(rootPath, 'assets', 'images', 'icon.ico')
+  });
+
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
--- Web App/assets/scripts/ip_addr.js
+++
@@ -1,1 +0,0 @@
-var ip_addr = "http://ce37-111-88-231-101.ngrok.io"
